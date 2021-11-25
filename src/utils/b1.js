00be0be448004a93c01e5979e95c41ca234f6e16
renjieonline <826348691@qq.com>
2021-11-25 19:12:27 +0800

    2 files modified

--- src/utils/b1.js
+++ src/utils/b1.js
@@ -1,7 +1,7 @@
-const { getNextReqId } = require("./a");
+const { getNextReqId } = require('./a1');
 
 module.exports = {
   testb1: () => {
-    console.log("b1:", getNextReqId());
-  }
+    console.log('b1:', getNextReqId());
+  },
 };
--- src/utils/c1.js
+++ src/utils/c1.js
@@ -1,7 +1,7 @@
-const { getNextReqId } = require("./a");
+const { getNextReqId } = require('./a1');
 
 module.exports = {
   testc1: () => {
-    console.log("c1:", getNextReqId());
-  }
+    console.log('c1:', getNextReqId());
+  },
 };
